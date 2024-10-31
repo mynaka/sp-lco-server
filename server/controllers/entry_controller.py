54e5cdab684e86c46dc86e3967dd42bdc2e1622b
mynaka <avillanueva@up.edu.ph>
2024-10-31 19:35:48 +0800

    feat: Tables

--- server/controllers/entry_controller.py
+++ server/controllers/entry_controller.py
@@ -1,12 +1,11 @@
 # controllers/entry_controller.py
 import csv
 from io import StringIO
-import os
-import re
-import json
+from typing import Dict, List
 from fastapi import APIRouter, File, Form, HTTPException, UploadFile, status, Depends
 from fastapi.security import OAuth2PasswordBearer
-import shutil
+
+from pydantic import BaseModel
 
 from database import get_neo4j_driver
 import json
@@ -23,53 +22,53 @@ router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+class DataInput(BaseModel):
+    prefLabel: str
+    identifier: str
+    description: str
+    format: str
+    sample: str
+    output: str
+
 # Create entry route
 @router.post("/create")
-async def create_entry(entry: Entry, current_user: dict = Depends(get_current_user)):
+async def create_entry(data: DataInput, current_user: dict = Depends(get_current_user)):
     """Create entry for Neo4J database"""
-    elements_str = json.dumps(entry.elements)  # Convert elements dictionary to string
 
     with get_neo4j_driver().session() as session:
-        # Check if the term_code is unique
+        # Check if the identifier is unique
         result = session.run(
-            "MATCH (e:Entry {term_code: $term_code}) RETURN e",
-            term_code=entry.term_code
+            "MATCH (e:Table {identifier: $identifier}) RETURN e",
+            identifier=data.identifier  # Fixed variable name
         )
         if result.single():
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Term code already exists")
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Identifier already exists")
 
         # Create the entry node
         result = session.run(
             """
-            CREATE (e:Entry {
-                name: $name,
-                term_code: $term_code,
-                elements: $elements
-            })
-            RETURN elementId(e) AS term_id, e.name AS name, e.term_code AS term_code, e.elements AS elements
+            CREATE (e:Table {prefLabel: $prefLabel, identifier: $identifier, description: $description, format: $format, sample: $sample, output: $output})
+            RETURN e
             """,
-            name=entry.name,
-            term_code=entry.term_code,
-            elements=elements_str
+            prefLabel=data.prefLabel,
+            identifier=data.identifier,
+            description=data.description,
+            format=data.format,
+            sample=data.sample,
+            output=data.output
         )
         created_entry = result.single()
         if not created_entry:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create entry")
 
-        # Create relationships for associated_terms
-        for relationship, term_codes in entry.associated_terms.items():
-            for term_code in term_codes:
-                session.run(
-                    f"""
-                    MATCH (e1:Entry {{term_code: $term_code1}}), (e2:Entry {{term_code: $term_code2}})
-                    CREATE (e1)-[:{relationship}]->(e2)
-                    """,
-                    term_code1=entry.term_code,
-                    term_code2=term_code,
-                )
-
-    return {"status": 200, "term": {created_entry["name"], created_entry["term_code"], created_entry["elements"]}}
-
+    return {
+        "status": 200,
+        "term": {
+            "name": created_entry["e"]["prefLabel"],
+            "identifier": created_entry["e"]["identifier"],
+            "description": created_entry["e"]["description"],
+        }
+    }
 @router.get("/all")
 async def get_all_entries():
     """Get all existing entries from all databases.
@@ -106,8 +105,10 @@ async def get_root_entries(database: str):
         # Neo4j query to fetch entries and their parent relationships
         query = (
             """
-            MATCH (e:Entity)
-            WHERE (e.notation STARTS WITH $database + ":" OR e.identifier STARTS WITH $database + ":") AND NOT((e)-[]->())
+            MATCH (e)
+            WHERE (e:Entity OR e:Table) AND 
+                (e.notation STARTS WITH $database + ":" OR e.identifier STARTS WITH $database + ":") AND 
+                NOT((e)-[]->())
             RETURN e.prefLabel AS prefLabel, 
                 COALESCE(e.notation, e.identifier) AS notation,
                 EXISTS(()-[]->(e)) AS hasIncomingRelationships,
@@ -146,64 +147,12 @@ async def get_root_entries(database: str):
         # Handle exceptions, log error, and return an error response
         return {"status": "500", "error": str(e)}
 
-
-@router.get("/database/{database}")
-async def get_root_entries(database: str):
-    """Get all entries from a given database. 
-    
-    Returns it in a Tree structure processable by PrimeVue.
-    """
-    try:
-        # Neo4j query to fetch entries and their parent relationships
-        db = database + ":"
-        query = (
-            """
-            MATCH (root:Entity)
-            WHERE (root.notation STARTS WITH $db OR root.identifier STARTS WITH $db) AND NOT((root)-[SUBSET_OF]->())
-            RETURN root.prefLabel AS prefLabel, 
-                root.notation AS notation, 
-                EXISTS(()-[]->(root)) AS hasIncomingRelationships,
-                root AS data
-            """
-        )
-
-        # Execute query
-        with get_neo4j_driver().session() as session:
-            result = session.run(query, database=db)
-
-            entry_dict = {}
-
-            for record in result:
-                entry = record["data"]
-                pref_label = record["prefLabel"]
-                notation = record["notation"]
-                has_incoming_relationships = record["hasIncomingRelationships"]
-                
-                # Create entry data
-                entry_data = {
-                    "key": notation,
-                    "label": pref_label,
-                    "data": entry,
-                    "leaf": not has_incoming_relationships,
-                    "loading": True
-                }
-                
-                # Store entry data
-                entry_dict[notation] = entry_data
-
-            root_entries = list(entry_dict.values())
-
-        return {"status": "200", "entries": root_entries}
-
-    except Exception as e:
-        return {"status": "500", "error": str(e)}
-
 @router.get("/database/{node_notation}/children")
 async def get_children(node_notation: str):
     """Get all children of the given node where a SUBCLASS_OF relationship exists."""
     query = """
     MATCH (child)-[:SUBCLASS_OF]->(parent)
-    WHERE parent.identifier = $node_notation OR parent.notation = $node_notation
+    WHERE (child:Entity OR child:Table) AND (parent.identifier = $node_notation OR parent.notation = $node_notation)
     RETURN child.prefLabel AS prefLabel,
         COALESCE(child.notation, child.identifier) AS notation,  // Use COALESCE to choose identifier if notation is null
         EXISTS(()-[]->(child)) AS hasIncomingRelationships,
